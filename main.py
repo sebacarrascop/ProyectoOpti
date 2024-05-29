64a18464ed225d9f63a21b961fa80af68060998e
octavioaguila <aguilaocta@gmail.com>
2024-05-28 22:49:43 -0400

    feat(FO and variables defined)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import gurobipy as gp
+from gurobipy import GRB, quicksum
 import pandas as pd
 import numpy as np
 import networkx as nx
@@ -11,6 +12,7 @@ path_to_data = os.path.join(os.getcwd(), 'simple')
 # Conjunto de nodos
 # nodo_id,x,y
 N = pd.read_csv(os.path.join(path_to_data, 'nodos.csv'))
+cantidad_nodos = len(N)
 
 # Electrolineras 
 # nodo_id,potencia_de_carga,carga_por_unidad_tiempo
@@ -43,6 +45,7 @@ V = pd.read_csv(os.path.join(path_to_data, 'tipo_auto.csv'))
 # Definimos conjunto de autos
 #id,tipo,temperatura_inicial,porcentaje_inicial_bateria
 C = pd.read_csv(os.path.join(path_to_data, 'autos.csv'))
+cantidad_autos = len(C)
 
 # Obtenemos parametros
 # porcentaje_max_bateria,porcentaje_min_bateria
@@ -50,9 +53,48 @@ parametros = pd.read_csv(os.path.join(path_to_data, 'parametros.csv'))
 B_max = parametros.iloc[0, 0]
 B_min = parametros.iloc[0, 1]
 
+
+
+
+### MODELO DE OPTIMIZACION ###
+
+m = gp.Model("electric_cars")
+
+
+## PARAMETROS ##
+
+# Matriz de incidencia tiempo de viaje entre nodos (SUME 1 PARA MANTENER QUE EL INDICE DE LOS NODOS PARTE DE 1)
+# por ende la primera fila y columna siempre seran 0.
+T_ij = np.zeros((cantidad_nodos + 1, cantidad_nodos + 1))
+for i, j, data in G.edges(data=True):
+    T_ij[i, j] = data['tiempo']
+
+## VARIABLES ## 
+
+# Se define variable X_ij: 1 si el nodo i y el nodo j pertenecen al camino elegido del auto k
+# Suponemos que cantidad_autos es el rango para k y G es tu grafo
+X_ijk = m.addVars(G.edges(), range(cantidad_autos), vtype=gp.GRB.BINARY, name="X")
+
+# Se define variable E_jk: energía porcentual de la batería del auto k al salir del nodo j.
+E_jk = m.addVars(G.nodes(), range(cantidad_autos), vtype=gp.GRB.CONTINUOUS, name="E")
+
+# Se define variable R_ik: tiempo de carga en el punto de carga i del auto k
+R_ik = m.addVars(G.nodes(), range(cantidad_autos), vtype=gp.GRB.CONTINUOUS, name="R")
+
+# Se define variable F_ik: temperatura de la batería del auto k en el nodo i
+F_ik = m.addVars(G.nodes(), range(cantidad_autos), vtype=gp.GRB.CONTINUOUS, name="F")
+
+# Se define la variable U_ik: 1 si la tempertura de la bateria del auto k supera la tempertura ideal de operación en el nodo i
+U_ik = m.addVars(G.nodes(), range(cantidad_autos), vtype=gp.GRB.BINARY, name="U")
+
+
+obj = gp.quicksum(gp.quicksum(gp.quicksum(gp.quicksum(T_ij[i, j] * X_ijk[i, j, k] for i, j in G.edges()) for k in range(cantidad_autos))))
+m.setObjective(obj, gp.GRB.MINIMIZE)
+
+
 # imprimimos el grafo con matplotlib
 # SIN ETIQUETAS
 # LAS ARISTAS SON DIRIGIDAS
-pos = nx.get_node_attributes(G, 'pos')
-nx.draw(G, pos, with_labels=False, node_size=100, node_color='blue', arrows=True, arrowstyle='-|>', arrowsize=10)
-plt.show()
+# pos = nx.get_node_attributes(G, 'pos')
+# nx.draw(G, pos, with_labels=False, node_size=100, node_color='blue', arrows=True, arrowstyle='-|>', arrowsize=10)
+# plt.show()
